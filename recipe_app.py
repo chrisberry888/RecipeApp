afd2b0699f4ed6000215b2b3da261a523835f573
Chris <chrisberry888@gmail.com>
2020-07-07 21:55:10 -0400

    made driver function

--- recipe_app.py
+++ recipe_app.py
@@ -4,23 +4,22 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 
-
 def add_recipes():
-    recipes = open("D:\Git_Repositories\RecipeApp\\recipe_file.txt", "w")
+    
     while True:
         another_recipe = input("Another recipe? Y/N ")
         if re.match("(N|n)o*", another_recipe):
             print("Done!")
             break
-        recipes.write("<recipe>\n")
+        
         
         name = input("Recipe name? ")
-        recipes.write("<name>%s</name>\n" % name)
+        
         
         servings = input("Servings? ")
-        recipes.write("<servings>%s</servings>\n" % servings)
         
-        recipes.write("<spices>\n")
+        
+        
         spices = []
         spices_amounts = []
         print("If no more spices, press enter")
@@ -31,10 +30,10 @@ def add_recipes():
             amount = input("amount: ")
             spices.append(spice)
             spices_amounts.append(amount)
-            recipes.write("%s, %s; " % (spice, amount))
-        recipes.write("\n</spices>\n")
+            
         
-        recipes.write("<ingredients>\n")
+        
+        
         ingredients = []
         ingredients_amounts = []
         print("If no more ingredients, press enter")
@@ -45,24 +44,17 @@ def add_recipes():
             amount = input("amount: ")
             ingredients.append(ingredient)
             ingredients_amounts.append(amount)
-            recipes.write("%s, %s; " % (ingredient, amount))
-        recipes.write("\n</ingredients>\n")
-        
-        recipes.write("<steps>\n")
+            
         steps = []
-        count = 1
+        
         print("If no more steps, press enter")
         while True:
             step = input("Step: ")
             if step == "":
                 break
             steps.append(step)
-            recipes.write("<%d>%s</%d>\n" % (count, step, count))
-            count += 1
-        recipes.write("</steps>\n")
-        
             
-        recipes.write("</recipe>\n")
+            
         
         new_recipe = {
             u'name': name,
@@ -75,20 +67,27 @@ def add_recipes():
         }
         
         db.collection(u'recipes').document(name).set(new_recipe)
+    
+    
+    
+
+
+def driver():
+    while True:
+        ans = input(
+            '''Select function:
+            \n1) Add Recipes
+            \n0) Quit\n''')
+        if ans == "1":
+            add_recipes()
+        elif ans == "0":
+            break
+        else:
+            print("invalid input")
         
-        
-    recipes.close()
-    
-    
+            
     
 
-
-
-
-def driver(){
-    
-    
-}
 
 cred = credentials.Certificate("D:\Secret_keys\super_secret_key.json")
 firebase_admin.initialize_app(cred)
