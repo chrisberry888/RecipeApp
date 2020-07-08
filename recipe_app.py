ef3a94b99f6c44de72b80bd8f1e45f95fa7f4040
Chris <chrisberry888@gmail.com>
2020-07-07 20:31:07 -0400

    incorporated firestore use into add_recipes

--- recipe_app.py
+++ recipe_app.py
@@ -21,32 +21,42 @@ def add_recipes():
         recipes.write("<servings>%s</servings>\n" % servings)
         
         recipes.write("<spices>\n")
+        spices = []
+        spices_amounts = []
         print("If no more spices, press enter")
         while True:
             spice = input("Spice: ")
             if spice == "":
                 break
             amount = input("amount: ")
+            spices.append(spice)
+            spices_amounts.append(amount)
             recipes.write("%s, %s; " % (spice, amount))
         recipes.write("\n</spices>\n")
         
         recipes.write("<ingredients>\n")
+        ingredients = []
+        ingredients_amounts = []
         print("If no more ingredients, press enter")
         while True:
             ingredient = input("Ingredient: ")
             if ingredient == "":
                 break
             amount = input("amount: ")
+            ingredients.append(ingredient)
+            ingredients_amounts.append(amount)
             recipes.write("%s, %s; " % (ingredient, amount))
         recipes.write("\n</ingredients>\n")
         
         recipes.write("<steps>\n")
+        steps = []
         count = 1
         print("If no more steps, press enter")
         while True:
             step = input("Step: ")
             if step == "":
                 break
+            steps.append(step)
             recipes.write("<%d>%s</%d>\n" % (count, step, count))
             count += 1
         recipes.write("</steps>\n")
@@ -54,39 +64,34 @@ def add_recipes():
             
         recipes.write("</recipe>\n")
         
+        new_recipe = {
+            u'name': name,
+            u'serving_size': servings,
+            u'spices': spices,
+            u'spices_amounts': spices_amounts,
+            u'ingredients': ingredients,
+            u'ingredients_amounts': ingredients_amounts,
+            u'steps': steps
+        }
+        
+        db.collection(u'recipes').document(name).set(new_recipe)
+        
         
     recipes.close()
     
-cred = credentials.Certificate("D:\Git_Repositories\RecipeApp\super_secret_key.json")
+    
+    
+
+
+
+
+def driver(){
+    
+    
+}
+
+cred = credentials.Certificate("D:\Secret_keys\super_secret_key.json")
 firebase_admin.initialize_app(cred)
-
-
 db = firestore.client()
 
-#add_recipes()
-
-doc_ref = db.collection(u'users').document(u'alovelace')
-doc_ref.set({
-    u'first': u'Ada',
-    u'last': u'Lovelace',
-    u'born': 1815
-})
-
-doc_ref = db.collection(u'users').document(u'aturing')
-doc_ref.set({
-    u'first': u'Alan',
-    u'middle': u'Mathison',
-    u'last': u'Turing',
-    u'born': 1912
-})
-
-
-users_ref = db.collection(u'users')
-docs = users_ref.stream()
-
-for doc in docs:
-    print(f'{doc.id} => {doc.to_dict()}')
-  
-  
-  
-
+driver()
